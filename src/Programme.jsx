8405064fedd3782bbd76152ffd565cbc5103437f
herimamy <fenohasinaherimamy@gmail.com>
2025-07-11 19:04:12 +0000

    envoie du programme

---
+++ src/Programme.jsx
@@ -0,0 +1,17 @@
+const Programmes = () => {
+    return (
+        <div className="w-screen bg-amber-900 h-[100vh] justify-center items-center">
+            <div className="flex flex-row h-[40vh] bg-amber-200 w-100vw ">
+                <div className="flex w-1/2 bg-red-400 "> 
+                    
+                </div>
+                 <div className="flex w-1/2 bg-red-400 "> 
+                <h2>Le programme pédagogique</h2>    
+                </div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Programmes
